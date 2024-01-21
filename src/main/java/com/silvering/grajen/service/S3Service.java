c0445d2cd32bb60dca90652d53698e4d44d599ca
silvering-steve <sstevenadi@gmail.com>
2024-01-21 16:10:13 +0700

    fixing import

--- src/main/java/com/silvering/grajen/service/S3Service.java
+++ src/main/java/com/silvering/grajen/service/S3Service.java
@@ -1,11 +1,9 @@
 package com.silvering.grajen.service;
 
-import com.silvering.grajen.model.FileModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
@@ -18,15 +16,12 @@ import java.util.Optional;
 public class S3Service {
     private final S3Client s3Client;
 
-    private final FileService fileService;
-
     @Value("${aws.bucketName}")
     private String bucketName;
 
     @Autowired
     public S3Service(S3Client s3Client, FileService fileService) {
         this.s3Client = s3Client;
-        this.fileService = fileService;
     }
 
     // Check if file uploaded successfully
